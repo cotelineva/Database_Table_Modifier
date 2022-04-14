53f2ca4107e31c985088b268b4ad12a5936d7665
cotelineva <91548656+cotelineva@users.noreply.github.com>
2022-04-14 17:11:15 +0300

    Add files via upload

---
+++ ParentChildRelation/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Tema1_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Connect DB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(347, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IdA:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(424, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(356, 32);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(546, 150);
+            this.dataGridView3.TabIndex = 4;
+            this.dataGridView3.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "NameV:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "VornameV:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "IdV:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(424, 254);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(120, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(424, 280);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(120, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(469, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Insert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(631, 336);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(550, 336);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(112, 239);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(213, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Message:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(377, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Gehalt:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(424, 306);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(120, 20);
+            this.textBox7.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(907, 371);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private FlorarieDataSet florarieDataSet;
+        private System.Windows.Forms.BindingSource adresseBindingSource;
+        private FlorarieDataSetTableAdapters.AdresseTableAdapter adresseTableAdapter;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private FlorarieDataSet1 florarieDataSet1;
+        private System.Windows.Forms.BindingSource verkauferBindingSource;
+        private FlorarieDataSet1TableAdapters.VerkauferTableAdapter verkauferTableAdapter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ ParentChildRelation/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Tema1_v2
+{
+    public partial class Form1 : Form
+    {      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlDataAdapter dAdapter;
+        DataSet dSet;
+        private string conStr = "Integrated Security=true;" +
+                                "Initial Catalog=Florarie;" +
+                                "Data Source=LAPTOP-N16I6V2I\\SQLEXPRESS;";
+        SqlConnection con = new SqlConnection("Integrated Security=true;" +
+                                "Initial Catalog=Florarie;" +
+                                "Data Source=LAPTOP-N16I6V2I\\SQLEXPRESS;");
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dAdapter = new SqlDataAdapter("select * from Adresse", conStr);
+            dSet = new System.Data.DataSet();
+            dAdapter.Fill(dSet);
+            dataGridView1.DataSource = dSet.Tables[0].DefaultView;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //exception handler for not binding the second pressed cell with the first one
+            if (textBox2.DataBindings.Count > 0)
+                textBox2.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt IdA in einem TextBox wenn ein cell gedruckt wird)
+            Binding b1 = new Binding("Text", dataGridView1[0, e.RowIndex], "Value", false);
+            textBox2.DataBindings.Add(b1);
+            textBox2.ReadOnly = true;
+
+            //zeigt die Verkaufer fur IdA der gedruckt wurde
+
+            //wir nehmen die Werte aus der Verkaufer Tabelle mit dem IdA = @p
+            dAdapter = new SqlDataAdapter("select * from Verkaufer where IdA = @p", conStr);    
+            dAdapter.SelectCommand.Parameters.Add("@p", SqlDbType.Int, 10).Value = dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+
+            //wir stellen die Tupeln in einem data set
+            dSet = new System.Data.DataSet();
+            dAdapter.Fill(dSet);
+
+            //stellt die Werte in den zweiten data grid
+            dataGridView3.DataSource = dSet.Tables[0].DefaultView;  
+        }
+
+        private void dataGridView3_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //exception handler for not binding the second pressed cell with the first one
+            if (textBox3.DataBindings.Count > 0)
+                textBox3.DataBindings.RemoveAt(0);
+
+            //binds cell to text box (zeigt NameV)
+            Binding b2 = new Binding("Text", dataGridView3[0, e.RowIndex], "Value", false);
+            textBox3.DataBindings.Add(b2);
+
+            //zeigt VornameV
+            if (textBox4.DataBindings.Count > 0)
+                textBox4.DataBindings.RemoveAt(0);
+            Binding b3 = new Binding("Text", dataGridView3[1, e.RowIndex], "Value", false);
+            textBox4.DataBindings.Add(b3);
+
+            //zeigt IdV
+            if (textBox5.DataBindings.Count > 0)
+                textBox5.DataBindings.RemoveAt(0);
+            Binding b4 = new Binding("Text", dataGridView3[2, e.RowIndex], "Value", false);
+            textBox5.DataBindings.Add(b4);
+
+            //zeigt Gehalt
+            if (textBox7.DataBindings.Count > 0)
+                textBox7.DataBindings.RemoveAt(0);
+            Binding b5 = new Binding("Text", dataGridView3[4, e.RowIndex], "Value", false);
+            textBox7.DataBindings.Add(b5);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {                      
+            //insert
+            
+            if (textBox5.Text.All(Char.IsDigit) && textBox7.Text.All(Char.IsDigit))
+            {
+                try { 
+                    SqlDataAdapter cmd = new SqlDataAdapter();
+                    cmd.InsertCommand = new SqlCommand("insert into Verkaufer values (@name,@vorname,@idV,@idA,@gehalt)", con);
+
+                    cmd.InsertCommand.Parameters.AddWithValue("@idV", textBox5.Text);
+                    cmd.InsertCommand.Parameters.AddWithValue("@name", textBox3.Text);
+                    cmd.InsertCommand.Parameters.AddWithValue("@vorname", textBox4.Text);
+                    cmd.InsertCommand.Parameters.AddWithValue("@idA", textBox2.Text);
+                    cmd.InsertCommand.Parameters.AddWithValue("@gehalt", textBox7.Text);
+
+                    con.Open();
+                    cmd.InsertCommand.ExecuteNonQuery();
+                    con.Close();
+
+                    textBox6.Text = "Inserted!";
+                }
+                catch(Exception exception){
+                    MessageBox.Show(exception.ToString(),"Error");
+                    con.Close();
+                }
+            }
+            else
+            {
+                textBox6.Text = "Error: Wrong DataType for IdV or Gehalt!";
+                MessageBox.Show("This field only for integers!", "Error");
+            }           
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //delete
+
+            SqlCommand cmd = new SqlCommand("delete from Verkaufer where IdV = @idV", con);
+
+            cmd.Parameters.AddWithValue("@idV", textBox5.Text);
+            
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            
+            textBox6.Text = "Deleted!";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //update
+
+            if (textBox5.Text.All(Char.IsDigit) && textBox7.Text.All(Char.IsDigit))
+            {
+                SqlCommand cmd = new SqlCommand("update Verkaufer set NameV = @name, VornameV = @vorname, Gehalt = @gehalt where IdV = @idV", con);
+
+                cmd.Parameters.AddWithValue("@idV", textBox5.Text);
+                cmd.Parameters.AddWithValue("@name", textBox3.Text);
+                cmd.Parameters.AddWithValue("@vorname", textBox4.Text);
+                cmd.Parameters.AddWithValue("@gehalt", textBox7.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                textBox6.Text = "Updated!";
+            }
+            else
+            {
+                textBox6.Text = "Error: Wrong DataType for IdV or Gehalt!";
+                MessageBox.Show("This field only for integers!", "Error");
+            }
+        }
+    }
+}
